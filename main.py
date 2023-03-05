4c54bffe64b1e1774261bad77fd5e3662e3b88e3
NikhilRaikar17 <nikhilraikar88@gmail.com>
2023-03-05 16:16:06 +0100

    recent changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, )
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app, resources={r"/home": {"origins": "http://127.0.0.1:5000"}})
@@ -10,7 +10,7 @@ CORS(app, resources={r"/home": {"origins": "http://127.0.0.1:5000"}})
 @app.route("/home")
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def home():
-    return render_template('index.html')
+    return render_template('dashboard.html')
 
 
 @app.route("/commentary")
